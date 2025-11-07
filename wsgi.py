82e839c7dfe46f45e2ab58009172f3d4b3abe63f
GILBERTO DOMINGUEZ <powlarregui1996@gmail.com>
2025-11-06 19:24:44 -0600

    Fix Vercel configuration and error handling

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for Frisol project.
-It exposes the WSGI callable as a module-level variable named ``application``.
-"""
 import os
 from app import app
-
-# Configuración para producción
-app.config['ENV'] = 'production'
-app.config['DEBUG'] = False
 
 # Asegurarse de que las rutas sean correctas
 app.static_folder = 'static'
 app.template_folder = 'templates'
 
+# Esto es necesario para Vercel
 application = app
